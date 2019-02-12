442ac1b324335488e72f053a233e407587ed101b
Noah Gilmore <noah.gilmore@autodesk.com>
2019-02-12 15:51:10 -0800

    App that reads the message

--- NGProjectThatRequestsNGResourcePodExample/ViewController.swift
+++ NGProjectThatRequestsNGResourcePodExample/ViewController.swift
@@ -12,7 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .white
+        let label = UILabel()
+        view.addSubview(label)
+        label.frame = CGRect(x: 40, y: 60, width: 200, height: 200)
+
+        guard let podBundleUrl = Bundle.main.url(forResource: "NGResourcePodExample", withExtension: "bundle"),
+            let podBundle = Bundle(url: podBundleUrl) else {
+                fatalError("Pod resource bundle not found")
+        }
+        let url = podBundle.url(forResource: "message", withExtension: "txt")!
+        guard let content = try? String(contentsOf: url) else {
+            fatalError("Could not read content string")
+        }
+        label.text = content
     }
 
 
